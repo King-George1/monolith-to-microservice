eee128949aa782ec4e24b7abb0d90885f918dd7e
King-George1 <82467459+King-George1@users.noreply.github.com>
2022-09-24 19:00:55 +0000

    New update

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -4,8 +4,9 @@
 
 export const environment = {
   production: false,
-  appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  appName: "Udagram",
+  apiHost:
+    "http://a7c91b40a9aef4802b83a6228eaf1593-1230995536.us-east-1.elb.amazonaws.com:8080/api/v0",
 };
 
 /*
