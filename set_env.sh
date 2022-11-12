912e10899d34ae96741adbbdbabc5907a657a3d3
King-George1 <82467459+King-George1@users.noreply.github.com>
2022-11-12 15:13:34 +0000

    updated new aws resources materials

--- set_env.sh
+++ set_env.sh
@@ -1,10 +1,10 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=dev2053802db
-export POSTGRES_PASSWORD=dev2053802db
-export POSTGRES_HOST=dev2053802db.cbqr0ykqf4yc.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=dev9667245db
+export POSTGRES_PASSWORD=dev9667245db
+export POSTGRES_HOST=dev9667245db.cpfmmjaatkzo.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::uda-2053802-dev
+export AWS_BUCKET=arn:aws:s3:::uda-9667245-dev
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=testing
